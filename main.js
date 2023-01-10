bf445a53a12b098336efe1b209c241473ec1d007
Ulisesx21 <75042766+Ulisesx21@users.noreply.github.com>
2023-01-10 00:18:40 -0300

    -

--- main.js
+++ main.js
@@ -1,95 +1,12 @@
-const btnRight = document.querySelector(".btn-right")
-const btnLeft = document.querySelector(".btn-left")
-const titulo = document.querySelector(".rec-titulo")
-const ocupacion = document.querySelector(".rec-ocup")
-const parrafo = document.querySelector(".rec-parrafo")
-const img = document.querySelector(".rec-img")
+
 const span = document.querySelector(".span-anim")
 const nameDivContainer = document.getElementById("name-div-container")
 
-const dot1 = document.querySelector(".dot1")
-const dot2 = document.querySelector(".dot2")
-const dot3 = document.querySelector(".dot3")
 
 let myName = ["U","l","i","s","e","s"];
 
 const colors = ["#e74c3c","#8e44ad","#3498db","#e67e22","#2ecc71"];
 
-dot2.style.background = "#eee";
-
-let c = 0;
-
-let frases = [  
-    {
-        frase:"Lo vi ayudando a una señora a cruzar la calle.",
-        src:"./images/Mark Zuckerberg.jpg",
-        nombre:"Mark Zuckerberg",
-        ocupacion:"CEO de Meta",
-    },
-    {
-        frase:"Me sorprendió lo rápido que toma un vaso de agua.",
-        src:"./images/Bill Gates.jpg",
-        nombre:"Bill Gates",
-        ocupacion:"Empresario",
-    },
-    {
-        frase:"Una vez me presto 10 pesos para comprar un Guaymallén.",
-        src:"./images/Elon Musk.jpg",
-        nombre:"Elon Musk",
-        ocupacion:"CEO de Tesla, Inc.",
-    }  
-]
-
-    img.src = frases[c].src
-    titulo.innerHTML = frases[c].nombre
-    ocupacion.innerHTML = frases[c].ocupacion
-    parrafo.innerHTML = `"${frases[c].frase}"`
-
-btnRight.addEventListener("click",()=>{
-    c++
-    if(c >= frases.length){
-        c = 0;
-    }
-    setTimeout(()=>{
-        img.src = frases[c].src
-        titulo.innerHTML = frases[c].nombre
-        ocupacion.innerHTML = frases[c].ocupacion
-        parrafo.innerHTML = `"${frases[c].frase}"`
-        colorDot(c)
-    },200)
-})
-
-btnLeft.addEventListener("click",()=>{
-    c--
-    if(c < 0){
-        c = frases.length-1;
-    }
-    setTimeout(()=>{
-        img.src = frases[c].src
-        titulo.innerHTML = frases[c].nombre
-        ocupacion.innerHTML = frases[c].ocupacion
-        parrafo.innerHTML = `"${frases[c].frase}"`
-        colorDot(c)
-    },200)
-})
-
-function colorDot(n){
-    if(n === 0){
-        dot1.style.background = "rgb(0, 0, 0)"
-        dot2.style.background = "#eee"
-        dot3.style.background = "rgb(0, 0, 0)"
-    }
-    if(n === 1){
-        dot1.style.background = "rgb(0, 0, 0)"
-        dot2.style.background = "rgb(0, 0, 0)"
-        dot3.style.background = "#eee"
-    }
-    if(n === 2){
-        dot1.style.background = "#eee"
-        dot2.style.background = "rgb(0, 0, 0)"
-        dot3.style.background = "rgb(0, 0, 0)"
-    }
-}
 
 let count = 0;
 setInterval(()=>{
@@ -105,7 +22,7 @@ setInterval(()=>{
 },500)
 
 document.querySelector(".go-top-container")
-.addEventListener("click", () =>{
+.addEventListener("click", () => {
     window.scrollTo({
         top: 0,
         behavior: "smooth"
